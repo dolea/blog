8e0e14ce5d350ed8a97924d619f549ec040428bb
Daniel Olea <dom.olea@gmail.com>
2015-11-10 17:41:49 +0100

    Test method for getDate

--- src/test/java/domain/PostTest.java
+++ src/test/java/domain/PostTest.java
@@ -3,6 +3,8 @@ package domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+
 public class PostTest {
 
     @Test
@@ -15,5 +17,12 @@ public class PostTest {
     public void getTitle_WhenTitleIsLorem_ShouldReturnLorem() {
         Post testPost = new Post("Daniel Olea", "Lorem", null);
         Assert.assertEquals("Lorem", testPost.getTitle());
+    }
+
+    @Test
+    public void getDate_WhenDateObjectPassed_ShouldReturnDateObject() {
+        Date dateTest = new Date();
+        Post testPost = new Post("Daniel Olea", "Lorem", dateTest);
+        Assert.assertEquals(dateTest, testPost.getDate());
     }
 }
