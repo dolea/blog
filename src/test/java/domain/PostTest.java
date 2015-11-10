6ca80e349c4922ae662f65c5a8213ea1051d19bd
Daniel Olea <dom.olea@gmail.com>
2015-11-10 17:35:58 +0100

    Test method for getAuthor

--- src/test/java/domain/PostTest.java
+++ src/test/java/domain/PostTest.java
@@ -1,4 +1,13 @@
 package domain;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PostTest {
+
+    @Test
+    public void getAuthor_WhenAuthorIsDaniel_Olea_ShouldReturnDaniel_Olea() {
+        Post testPost = new Post("Daniel Olea", "", null);
+        Assert.assertEquals("Daniel Olea", testPost.getAuthor());
+    }
 }
