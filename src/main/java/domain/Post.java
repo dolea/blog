94f99d5498db47b4b4ba48eabc85fbe084765c6c
Daniel Olea <dom.olea@gmail.com>
2015-11-10 17:36:36 +0100

    Passing getAuthor method

--- src/main/java/domain/Post.java
+++ src/main/java/domain/Post.java
@@ -8,4 +8,13 @@ public class Post {
     private String title;
     private Date date;
 
+    public Post(String author, String title, Date date) {
+        this.author = author;
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
