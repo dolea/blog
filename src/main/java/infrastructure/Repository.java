c4ef1fca811a1b839532f3d17e214a83a4e6ed1f
Daniel Olea <dom.olea@gmail.com>
2015-11-12 09:09:36 +0100

    Passing tests for persitence

--- src/main/java/infrastructure/Repository.java
+++ src/main/java/infrastructure/Repository.java
@@ -2,29 +2,62 @@ package infrastructure;
 
 import domain.Post;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 
 public class Repository {
 
-    private Map<String, String> posts = new HashMap<>();
+    private static final String DEFAULT_PATH = "./repo.txt";
+
+    private OpenOption[] openOptions;
+    private String path;
+
+    public Repository(){
+        this(DEFAULT_PATH, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+    }
+
+    public Repository(String path, OpenOption... options) {
+        this.path = path;
+        openOptions = options;
+    }
 
     public String save(String title, String text) {
-        posts.put(title, text);
+        String post = title + '\0' + text + '\n';
+        try {
+            Files.write(Paths.get(path), post.getBytes(), openOptions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return title;
     }
 
     public List<Post> readAll() {
         List<Post> texts = new ArrayList<>();
-        for (String key: posts.keySet()) {
-            texts.add(this.read(key));
+
+        try {
+            for (String line : Files.readAllLines(Paths.get(path), Charset.defaultCharset())) {
+                String[] parts = line.split("\0");
+                texts.add(new Post(parts[0], parts[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return texts;
     }
 
     public Post read(String title) {
-        if(posts.containsKey(title)) {
-            return new Post(title, posts.get(title));
+        List<Post> posts = this.readAll();
+
+        for(Post post: posts) {
+            if(post.getTitle().equals(title)) {
+                return post;
+            }
         }
         return null;
     }
--- src/test/java/infrastructure/RepositoryTest.java
+++ src/test/java/infrastructure/RepositoryTest.java
@@ -1,15 +1,28 @@
 package infrastructure;
 
-import domain.Post;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.nio.file.StandardOpenOption;
 
 public class RepositoryTest {
 
+    private Repository repo;
+
+    @Before
+    public void initialize() {
+        repo = new Repository(
+                "./test_repo.txt",
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING
+        );
+    }
+
     @Test
     public void save() {
-        Repository repo = new Repository();
         String text = "Lorem";
         String title = "Ipsum";
         String id = repo.save(title, text);
@@ -18,16 +31,29 @@ public class RepositoryTest {
 
     @Test
     public void readInvalidTitle() {
-        Repository repo = new Repository();
         Assert.assertNull(repo.read("invalid"));
     }
 
     @Test
     public void readValidTitle() {
-        Repository repo = new Repository();
         String text = "Dolor";
         String title = "Sit";
         repo.save(title, text);
         Assert.assertThat(repo.read(title).getText(), Matchers.equalTo(text));
+    }
+
+    @Test
+    public void readValidTitle_GivenTwoPost() {
+        Repository repo2 = new Repository(
+                "./test_repo.txt",
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+        );
+
+        String text = "Xaz";
+        String title = "zaX";
+        repo2.save(title, text);
+        repo2.save("xxx", "zzz");
+        Assert.assertThat(repo2.read(title).getText(), Matchers.equalTo(text));
     }
 }
