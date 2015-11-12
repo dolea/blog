64e5c2c6942b0f138a47a5ede2c66be37afe157d
Daniel Olea <dom.olea@gmail.com>
2015-11-12 09:32:11 +0100

    Passing test

--- src/test/java/domain/PublishPostUseCaseTest.java
+++ src/test/java/domain/PublishPostUseCaseTest.java
@@ -5,14 +5,15 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
+import java.nio.file.StandardOpenOption;
 
 public class PublishPostUseCaseTest {
+    private static final String REPO_PATH = "./test_repo.txt";
+    private Post post = new Post("first title", "first text");
+
     @Test
     public void publishPost() {
-        Post post = new Post("first title", "first text");
-
-        Repository repo = new Repository();
+        Repository repo = new Repository(REPO_PATH, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
         PublishPostUseCase publishPostUc = new PublishPostUseCase(repo);
         String id = publishPostUc.publish(post.getTitle(), post.getText());
 
@@ -20,17 +21,15 @@ public class PublishPostUseCaseTest {
     }
 
     @Test
-    public void publishTwoPosts() {
-        Post post1 = new Post("first title", "first text");
+    public void publish_AppendNewPost() {
         Post post2 = new Post("second title", "second text");
 
-        Repository repo = new Repository();
+        Repository repo = new Repository(REPO_PATH, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
         PublishPostUseCase publishPostUc = new PublishPostUseCase(repo);
 
-        publishPostUc.publish(post1.getTitle(), post1.getText());
         publishPostUc.publish(post2.getTitle(), post2.getText());
 
         //TODO: order dependant
-        Assert.assertThat(repo.readAll(), Matchers.<Post>contains(post2, post1));
+        Assert.assertThat(repo.readAll(), Matchers.<Post>contains(post, post2));
     }
 }
